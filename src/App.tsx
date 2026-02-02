aa65427925fd3b8813da41faf43efbecad525416
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2026-02-02 16:50:32 +0900

    [FEAT] App.tsx 페이지 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+import NotFound from './pages/NotFound'
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound /> } />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
