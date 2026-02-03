0ec338f8c354fe4d00249803b7638b9189205fee
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2026-02-04 00:44:09 +0900

    [FEAT] Header 구조 조정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,15 @@
+import styled from "styled-components"
 
+const Header = () => {
+  const HeaderWrapper = styled.header`
+    height: auto;
+    position: sticky;
+    top: 0;
+    
+    padding: 32px 64px 24px;
+  `
 
-export default function Header() {
-  return <h1>Header</h1>
+  return <HeaderWrapper>Header</HeaderWrapper>
 }
+
+export default Header
