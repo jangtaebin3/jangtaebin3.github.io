9edf51423c207b1140df0786e0ebde2c78c6b69c
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2026-02-04 00:25:48 +0900

    [FEAT] Layout 컴포넌트 구조 설정

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,16 +1,27 @@
 import { Outlet } from 'react-router-dom'
+import styled from 'styled-components'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
+const LayoutWrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const Main = styled.main`
+  flex: 1;
+`
+
 const Layout = () => {
   return (
-    <div className="layout">
+    <LayoutWrapper>
       <Header />
-      <main className="main">
+      <Main>
         <Outlet />
-      </main>
+      </Main>
       <Footer />
-    </div>
+    </LayoutWrapper>
   )
 }
 
