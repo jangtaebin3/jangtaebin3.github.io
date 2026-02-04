29d355aeabe5dc30ca9f98cce0917b326ab132d6
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2026-02-05 01:10:38 +0900

    [FEAT] Footer Contact 기능 제작

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,40 @@
+import { FaGithub, FaInstagram } from 'react-icons/fa'
+import { MdEmail } from 'react-icons/md'
+import { FooterWrapper, IconList, IconLink } from './style'
 
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <IconList>
+        <IconLink
+          href="https://github.com/jangtaebin3"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+        >
+          <FaGithub size={32} />
+        </IconLink>
 
-export default function Footer() {
-  return <h1>Footer</h1>
+        <IconLink
+          href="https://instagram.com/j_tae.b_0416"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Instagram"
+        >
+          <FaInstagram size={32} />
+        </IconLink>
+
+        <IconLink
+          href="mailto:your-email@gmail.com"
+          aria-label="Email"
+        >
+          <MdEmail size={32} />
+        </IconLink>
+      </IconList>
+
+      <small>© 2026 Jang Taebin</small>
+    </FooterWrapper>
+  )
 }
+
+export default Footer
