2340926d8ba81ce44b80e2257134734c5dd159bd
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2026-02-05 00:10:40 +0900

    [STYLE] NavItem 디자인 추가

--- src/components/NavBar/style.ts
+++ src/components/NavBar/style.ts
@@ -21,13 +21,45 @@ export const NavItems = styled.div`
 `
 
 export const NavItem = styled(NavLink)`
+  position: relative;
+  padding: 4px 0;
+
   text-decoration: none;
-  color: #333;
+  color: #666;
   font-size: 24px;
   font-weight: 500;
+
+  transition: color 0.2s ease;
+
+  &::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: -2px;
+
+    width: 100%;
+    height: 2px;
+    background-color: #000;
+
+    transform: scaleX(0);
+    transform-origin: left;
+    transition: transform 0.25s ease;
+  }
+
+  &:hover {
+    color: #000;
+  }
+
+  &:hover::after {
+    transform: scaleX(1);
+  }
 
   &.active {
     color: #000;
     font-weight: 700;
+  }
+
+  &.active::after {
+    transform: scaleX(1);
   }
 `
