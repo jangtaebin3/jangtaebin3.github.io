1299fe6deed375fa57e21c59eb849512e8d8fba5
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2026-02-08 23:49:38 +0900

    [CHORE] Keyword section 분리

---
+++ src/pages/About/Keywords.tsx
@@ -0,0 +1,18 @@
+import * as S from './style';
+import AnimatedSection from './AnimatedSection';
+
+const Keywords = () => {
+  return (
+    <AnimatedSection>
+      <S.Section>
+        <S.SectionTitle>Keywords</S.SectionTitle>
+        <S.ValueContent>
+          Problem-first Thinking · Structured Communication · Frontend Development ·<br />
+          Academic Research · Project Management · Learning-oriented
+        </S.ValueContent>
+      </S.Section>
+    </AnimatedSection>
+  )
+}
+
+export default Keywords;
