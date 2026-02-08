2c22c4f7a0f0f84bf70fea8a529506a77a602031
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2026-02-08 19:30:53 +0900

    [STYLE] Intro 디자인 적용

--- src/pages/About/style.ts
+++ src/pages/About/style.ts
@@ -2,4 +2,35 @@ import styled from 'styled-components';
 
 export const AboutContainer = styled.div`
   padding: 64px 120px;
+  background-color: #F8FBF4;
+`;
+
+export const AboutTitle = styled.p`
+  color: #000;  
+  font-size: 64px;
+  font-style: normal;
+  font-weight: 700;
+  margin-bottom: 80px;
+`;
+
+export const IntroBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 108px;
+`;
+
+export const IntroText = styled.p`
+  color: #000;
+  font-size: 48px;
+  font-style: normal;
+  font-weight: 700;
+  margin-bottom: 48px;
+`;
+
+export const IntroDetail = styled.p`
+  color: #70716E;
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 500;
+  margin-bottom: 12px;
 `;
