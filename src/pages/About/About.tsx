e67d1c3c6b76a73968bbbd2ce5af47ef0f5f5fab
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2026-02-08 23:40:15 +0900

    [FEAT] Title 애니메이션 적용

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,14 +1,33 @@
 import * as S from './style';
+import { motion } from 'framer-motion';
 import Intro from './Intro';
 import Experience from './Experience';
 import Education from './Education';
 import Skills from './Skills';
 import Value from './Value';
 
+const MotionTitle = motion(S.AboutTitle);
+
+export const titleVariants = {
+  hidden: { opacity: 0, x: 20 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.6,
+    },
+  },
+}
+
 const About = () => {
   return (
     <S.AboutContainer>
-      <S.AboutTitle>About me</S.AboutTitle>
+      <MotionTitle
+        variants={titleVariants}
+        initial="hidden"
+        animate="visible">
+          About me
+      </MotionTitle>
       <Intro />
       <Experience />
       <Education />
