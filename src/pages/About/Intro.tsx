6d0639e7f47f51441ada95407f189a29e3faf587
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2026-02-08 23:29:11 +0900

    [FEAT] Intro section 애니메이션 적용

--- src/pages/About/Intro.tsx
+++ src/pages/About/Intro.tsx
@@ -1,12 +1,44 @@
 import * as S from './style';
+import { motion } from 'framer-motion';
+
+const MotionSection = motion(S.Section)
+const MotionTitle = motion(S.SectionTitle)
+const MotionDetail = motion(S.IntroDetail)
+
+export const container = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.3,
+    },
+  },
+}
+
+export const item = {
+  hidden: { opacity: 0, y: 16 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+}
 
 const Intro = () => {
   return (
-    <S.Section>
-      <S.SectionTitle>안녕하세요, 개발자 장태빈입니다.</S.SectionTitle>
-      <S.IntroDetail>기능 구현보다 구조와 흐름을 먼저 고민합니다.</S.IntroDetail>
-      <S.IntroDetail>읽기 쉬운 코드와 유지보수가 쉬운 UI를 만드는 것을 중요하게 생각합니다.</S.IntroDetail>
-    </S.Section>
+    <MotionSection
+      variants={container}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+    >
+      <MotionTitle variants={item}>
+        안녕하세요, 개발자 장태빈입니다.
+      </MotionTitle>
+
+      <MotionDetail variants={item}>
+        기능 구현보다 구조와 흐름을 먼저 고민합니다.
+      </MotionDetail>
+
+      <MotionDetail variants={item}>
+        읽기 쉬운 코드와 유지보수가 쉬운 UI를 만드는 것을 중요하게 생각합니다.
+      </MotionDetail>
+    </MotionSection>
   )
 }
 
