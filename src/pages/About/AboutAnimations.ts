6f1b3046c63c6a24d2696c1f492a91e4dbf096de
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2026-02-09 00:21:38 +0900

    [CHORE] 애니메이션 분리

---
+++ src/pages/About/AboutAnimations.ts
@@ -0,0 +1,41 @@
+export const titleVariants = {
+  hidden: { opacity: 0, x: 20 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.6,
+    },
+  },
+}
+
+export const container = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.3,
+    },
+  },
+}
+
+export const item = {
+  hidden: { opacity: 0, y: 16 },
+  visible: { 
+    opacity: 1, 
+    y: 0, 
+    transition: { 
+      duration: 0.6,
+    }, 
+  },
+}
+
+export const sectionVariants = {
+  hidden: { opacity: 0, x: 64 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.9,
+    },
+  },
+}
