e8ef89335a2f5c77ba44888f8af438aebbde9316
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2026-02-10 17:31:47 +0900

    [CHORE] IntroHero 대체 적용

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,25 +1,15 @@
 import * as S from './style';
-import { motion } from 'framer-motion';
-import { titleVariants } from './AboutAnimations';
-import Intro from './IntroHero/Intro';
+import IntroHero from './IntroHero';
 import Experience from './AboutContent/Experience';
 import Education from './AboutContent/Education';
 import Skills from './AboutContent/Skills';
 import Value from './AboutContent/Value';
 import Keywords from './AboutContent/Keywords';
 
-const MotionTitle = motion(S.AboutTitle);
-
 const About = () => {
   return (
     <S.AboutContainer>
-      <MotionTitle
-        variants={titleVariants}
-        initial="hidden"
-        animate="visible">
-          About me
-      </MotionTitle>
-      <Intro />
+      <IntroHero />
       <Experience />
       <Education />
       <Skills />
