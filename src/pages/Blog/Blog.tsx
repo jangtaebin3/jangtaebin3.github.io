97d95bed55684e4f3a2bb8c726879f3870e94969
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2026-02-11 01:57:53 +0900

    [FEAT] BlogHeader 제작

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -1,7 +1,17 @@
 import * as S from './style';
 
 const Blog = () => {
-  return <div>Blog Page</div>;
+  return (
+    <S.BlogHeader>
+      <S.Background />
+      <S.Content>
+        <S.Title>Blog</S.Title>
+        <S.Description>
+          생각을 정리하고, 선택의 이유를 기록합니다.
+        </S.Description>
+      </S.Content>
+    </S.BlogHeader>
+  );
 }
 
 export default Blog;
