03c007612e4f2a683f8529aeb7d8d45f1ee0be52
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2026-02-16 23:19:35 +0900

    feat: BlogContainer styled 컴포넌트 추가

--- src/pages/Blog/style.ts
+++ src/pages/Blog/style.ts
@@ -1,2 +1,9 @@
 import styled from 'styled-components';
 
+export const BlogContainer = styled.div`
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;  
+  padding: 0 20vw;
+  gap: 32px;
+`;
