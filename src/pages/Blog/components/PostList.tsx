4519b44d4f32c8f8cdc84431ed8fc4ee11f187c6
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2026-02-17 22:30:06 +0900

    feat: PostItem 클릭 시 Detail 이동 기능 추가

--- src/pages/Blog/components/PostList.tsx
+++ src/pages/Blog/components/PostList.tsx
@@ -1,15 +1,22 @@
+import { useNavigate } from 'react-router-dom';
 import type { BlogPost } from '@/types/blog';
 import * as S from './style';
 
 interface PostListProps {
-  posts: BlogPost[]
+  post: BlogPost[]
 }
 
-const PostList = ({ posts }: PostListProps) => {
+const PostList = ({ post }: PostListProps) => {
+  const navigate = useNavigate()
+
+  const handleClick = (post: BlogPost) => {
+    navigate(`/blog/${post.category}/${post.slug}`)
+  }
+
   return (
     <S.PostListContainer>
-      {posts.map(post => (
-        <S.PostItem key={post.id}>
+      {post.map(post => (
+        <S.PostItem key={post.id} onClick={() => handleClick(post)}>
           <S.PostTitle>{post.title}</S.PostTitle>
           <S.PostDescription>{post.description}</S.PostDescription>
           <S.PostDate>{post.date}</S.PostDate>
