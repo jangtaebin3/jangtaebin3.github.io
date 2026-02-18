8b19b2af9af5b7c85570e003c3f95104c162032f
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2026-02-18 20:24:20 +0900

    feat: Description 내용 수정

--- src/pages/Blog/components/PostList.tsx
+++ src/pages/Blog/components/PostList.tsx
@@ -6,7 +6,14 @@ interface PostListProps {
   post: BlogPost[]
 }
 
-const normalizeDescription = (text: string) => text.replace(/\s*\n+\s*/g, ' ').trim()
+const normalizeDescription = (text: string) =>
+  text
+    .replace(/\s*\n+\s*/g, ' ')
+    .replace(/([.!?…])(?=[^\s])/g, '$1 ')
+    .replace(/([가-힣])([A-Za-z0-9])/g, '$1 $2')
+    .replace(/([A-Za-z0-9])([가-힣])/g, '$1 $2')
+    .replace(/\s{2,}/g, ' ')
+    .trim()
 
 const formatPostDate = (value: string) => {
   const date = new Date(value)
@@ -41,7 +48,7 @@ const PostList = ({ post }: PostListProps) => {
       {post.map(post => (
         <S.PostItem key={post.id} onClick={() => handleClick(post)}>
           <S.PostTitle>{post.title}</S.PostTitle>
-          <S.PostDescription>{normalizeDescription(post.description)}</S.PostDescription>
+          <S.PostDescription>{normalizeDescription(post.description)}...</S.PostDescription>
           <S.PostDate>{formatPostDate(post.date)}</S.PostDate>
         </S.PostItem>
       ))}
