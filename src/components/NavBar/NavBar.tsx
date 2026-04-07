b7aa9d3994d480e320b875bf287e269d8b56fd5e
jangtaebin3 <jtbc0416@soongsil.ac.kr>
2026-04-07 17:09:18 +0900

    feat: Home 에 있는 경우에만 맞춰 디자인 적용 기능 구현

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,14 +1,18 @@
 import { NavWrapper, Title, NavItems, NavItem } from './style'
+import { useLocation } from 'react-router-dom'
 
 const NavBar = () => {
+  const location = useLocation()
+  const isHome = location.pathname === '/'
+
   return (
     <NavWrapper>
-      <Title to="/">Jang Taebin</Title>
+      <Title to="/" $isHome={isHome}>Jang Taebin</Title>
       <NavItems>
-        <NavItem to="/" end>Home</NavItem>
-        <NavItem to="/about">About</NavItem>
-        <NavItem to="/projects">Projects</NavItem>
-        <NavItem to="/blog">Blog</NavItem>
+        <NavItem to="/" end $isHome={isHome}>Home</NavItem>
+        <NavItem to="/about" $isHome={isHome}>About</NavItem>
+        <NavItem to="/projects" $isHome={isHome}>Projects</NavItem>
+        <NavItem to="/blog" $isHome={isHome}>Blog</NavItem>
       </NavItems>
     </NavWrapper>
   )
